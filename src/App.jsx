400c356c735e5e36d00a8ffe6e3f25b3851e6581
MuhammedZeki <muhammed.codes@gmail.com>
2025-11-24 19:27:10 +0300

    Explore components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Explore from "./pages/Explore";
 import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
     <div>
       <HomePage />
+      <Explore />
     </div>
   );
 };
