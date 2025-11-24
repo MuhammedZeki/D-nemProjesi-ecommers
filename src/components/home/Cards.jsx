a837229ebeaeabeb5f7da9f9806b8e8001138758
MuhammedZeki <muhammed.codes@gmail.com>
2025-11-24 17:38:56 +0300

    cards added

--- src/components/home/Cards.jsx
+++ src/components/home/Cards.jsx
@@ -1,7 +1,24 @@
-import React from "react";
-
+import { CiUser, CiSearch, CiShoppingBasket, CiHeart } from "react-icons/ci";
 const Cards = () => {
-  return <div>Cards</div>;
+  return (
+    <div className="font-montserrat font-bold text-md leading-6 tracking-[0.2px] text-[#23A6F0] flex items-center gap-4 cursor-pointer">
+      <div className="flex items-center gap-1">
+        <CiUser className="w-6 h-6" />
+        <span>Login/Register</span>
+      </div>
+      <div>
+        <CiSearch className="w-6 h-6" />
+      </div>
+      <div className="flex items-center gap-2">
+        <CiShoppingBasket className="w-6 h-6" />
+        <span className="font-normal">1</span>
+      </div>
+      <div className="flex items-center gap-2">
+        <CiHeart className="w-6 h-6" />
+        <span className="font-normal">1</span>
+      </div>
+    </div>
+  );
 };
 
 export default Cards;
