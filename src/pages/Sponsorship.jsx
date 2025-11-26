3cc72edcf4dc743b26afab16203ea7175456ad74
MuhammedZeki <muhammed.codes@gmail.com>
2025-11-26 17:16:45 +0300

    sponsorship  mobile done

--- src/pages/Sponsorship.jsx
+++ src/pages/Sponsorship.jsx
@@ -9,7 +9,7 @@ import { DiEnvato } from "react-icons/di";
 const Sponsorship = () => {
   return (
     <div className=" bg-[#FAFAFA]">
-      <div className=" flex gap-4 w-[75%] mx-auto py-24 px-4">
+      <div className="lg:flex lg:flex-row lg:gap-4 w-[75%] mx-auto lg:py-24 lg:px-4 flex flex-col py-10 items-center lg:items-start">
         <FaHooli className="text-[#737373] h-36 w-36 grow" />
         <FaLyft className="text-[#737373] h-36 w-36 grow" />
         <DiEnvato className="text-[#737373] h-36 w-36 grow" />
