ae780fb8f55f919e5f5d54efd85c52629156d211
MuhammedZeki <muhammed.codes@gmail.com>
2025-11-26 16:44:58 +0300

    explore  mobile done

--- src/components/home/UpperComponets/MenuBar.jsx
+++ src/components/home/UpperComponets/MenuBar.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const MenuBar = () => {
   return (
     <div className="absolute z-10 top-23 left-0  bg-white  w-full lg:hidden flex flex-col items-center justify-center gap-6 text-[#737373] font-montserrat font-normal text-3xl leading-11 tracking-[0.2px] py-12">
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -3,68 +3,62 @@ const Explore = () => {
     <div className="bg-[#FAFAFA] p-6 -mt-2">
       <div className="w-[75%] mx-auto flex items-center justify-between gap-4 flex-wrap">
         <div className="border grow border-[#ECECEC] flex items-center bg-white px-4 py-2 font-montserrat justify-center">
-          <div className="flex flex-col gap-4">
-            <span className="font-bold text-xl leading-3.5 tracking-[0.2px text-[#737373]">
+          <div className="flex flex-col gap-2">
+            <span className="font-bold text-xl leading-6 tracking-[0.2px] text-[#737373]">
               Your Space
             </span>
-            <h2 className="font-bold text-2xl  leading-8 tracking-[0.1px] text-[#252B42]">
+            <h2 className="font-bold text-2xl leading-8 tracking-[0.1px] text-[#252B42]">
               Unique Life
             </h2>
             <h6 className="font-normal text-md  leading-4 tracking-[0.2px] text-[#252B42]">
-              Explore Imtes
+              Explore Items
             </h6>
           </div>
           <div>
-            {" "}
             <img
               src="/explore/caramel.png
             "
-              alt=""
-              className="h-52"
+              className="object-contain h-52"
             />
           </div>
         </div>
         <div className="border grow border-[#ECECEC] flex items-center bg-white px-4 py-2 font-montserrat justify-center">
-          <div className="flex flex-col gap-4">
-            <span className="font-bold text-xl leading-3.5 tracking-[0.2px text-[#737373]">
+          <div className="flex flex-col gap-2">
+            <span className="font-bold text-xl leading-6 tracking-[0.2px] text-[#737373]">
               Your Space
             </span>
             <h2 className="font-bold text-2xl  leading-8 tracking-[0.1px] text-[#252B42]">
               Unique Life
             </h2>
             <h6 className="font-normal text-md  leading-4 tracking-[0.2px] text-[#252B42]">
-              Explore Imtes
+              Explore Items
             </h6>
           </div>
           <div>
-            {" "}
             <img
               src="/explore/elma.jpg
             "
-              alt=""
-              className="h-52"
+              className="h-52 object-contain"
             />
           </div>
         </div>
         <div className="border grow border-[#ECECEC] flex items-center bg-white px-4 py-2 font-montserrat justify-center">
-          <div className="flex flex-col gap-4">
-            <span className="font-bold text-xl leading-3.5 tracking-[0.2px text-[#737373]">
+          <div className="flex flex-col gap-2">
+            <span className="font-bold text-xl leading-6 tracking-[0.2px] text-[#737373]">
               Your Space
             </span>
             <h2 className="font-bold text-2xl  leading-8 tracking-[0.1px] text-[#252B42]">
               Unique Life
             </h2>
             <h6 className="font-normal text-md  leading-4 tracking-[0.2px] text-[#252B42]">
-              Explore Imtes
+              Explore Items
             </h6>
           </div>
           <div>
-            {" "}
             <img
               src="/explore/et.jpg
             "
-              alt=""
-              className="h-52"
+              className="h-52 object-contain"
             />
           </div>
         </div>
