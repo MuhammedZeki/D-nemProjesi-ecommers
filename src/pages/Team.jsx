05b3b9bb8391c4e2163efb62c9f8f8f737ba6ab1
MuhammedZeki <muhammed.codes@gmail.com>
2025-11-28 17:50:18 +0300

    Team mobile is done

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -2,8 +2,8 @@ import TeamItem from "../components/team/TeamItem";
 
 const Team = () => {
   return (
-    <div className="lg:w-[75%] flex flex-col gap-12 lg:mx-auto lg:py-8  lg:px-4  font-montserrat">
-      <div className="flex flex-col gap-3 mt-12 items-center justify-center">
+    <div className="lg:w-[75%] flex flex-col p-5 gap-12 lg:mx-auto lg:py-8  lg:px-4  font-montserrat">
+      <div className="flex text-center lg:text-start flex-col gap-3 mt-12 items-center justify-center">
         <h2 className="font-bold text-4xl leading-12 tracking-[0.2px] text-[#252B42]">
           Meet Our Team
         </h2>
@@ -12,8 +12,8 @@ const Team = () => {
           of Classical physics: Newtonian mechanics{" "}
         </p>
       </div>
-      <div className="flex flex-col gap-4">
-        <div className="flex items-center gap-6">
+      <div className="flex flex-col gap-4 p-5">
+        <div className="lg:flex lg:flex-row flex flex-col items-center gap-6">
           <TeamItem img={"/team/team1.jpg"} name={"Jerome Bell"} job={"IBM"} />
           <TeamItem
             img={"/team/team2.jpg"}
@@ -31,7 +31,7 @@ const Team = () => {
             job={"Facebook"}
           />
         </div>
-        <div className="flex items-center gap-6">
+        <div className="lg:flex lg:flex-row flex flex-col items-center gap-6 ">
           <TeamItem
             img={"/team/team5.jpg"}
             name={"Jane Cooper"}
