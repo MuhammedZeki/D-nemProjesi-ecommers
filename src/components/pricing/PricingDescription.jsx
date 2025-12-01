5e79b62afc927826cf70c78ede1ffbb8d7e60f34
MuhammedZeki <muhammed.codes@gmail.com>
2025-12-01 17:24:22 +0300

    PricingDescription responsive is done

--- src/components/pricing/PricingDescription.jsx
+++ src/components/pricing/PricingDescription.jsx
@@ -13,8 +13,8 @@ const PricingDescription = () => {
           </p>
         </div>
         <div className="flex flex-col ">
-          <div className="flex items-center gap-8">
-            <div className="flex gap-2 px-16 py-8">
+          <div className="flex flex-col lg:flex lg:flex-row lg:items-center gap-8">
+            <div className="flex gap-2 lg:px-16 py-8">
               <MdKeyboardArrowRight className="text-[#23A6F0] w-25 h-25" />
               <div className="flex flex-col gap-2">
                 <h5 className="font-bold leading-6 tracking-[0.1px] text-[#252B42]">
@@ -27,35 +27,7 @@ const PricingDescription = () => {
                 </h6>
               </div>
             </div>
-            <div className="flex gap-2 px-16 py-8">
-              <MdKeyboardArrowRight className="text-[#23A6F0] w-25 h-25" />
-              <div className="flex flex-col gap-2">
-                <h5 className="font-bold leading-6 tracking-[0.1px] text-[#252B42]">
-                  the quick fox jumps over the lazy dog
-                </h5>
-                <h6 className="text-[#737373] font-medium leading-5 tracking-[0.2px]">
-                  Met minim Mollie non desert Alamo est sit cliquey dolor do met
-                  sent. RELIT official consequent door ENIM RELIT Mollie.
-                  Excitation venial consequent sent nostrum met.
-                </h6>
-              </div>
-            </div>
-          </div>
-          <div className="flex items-center gap-8">
-            <div className="flex gap-2 px-16 py-8">
-              <MdKeyboardArrowRight className="text-[#23A6F0] w-25 h-25" />
-              <div className="flex flex-col gap-2">
-                <h5 className="font-bold leading-6 tracking-[0.1px] text-[#252B42]">
-                  the quick fox jumps over the lazy dog
-                </h5>
-                <h6 className="text-[#737373] font-medium leading-5 tracking-[0.2px]">
-                  Met minim Mollie non desert Alamo est sit cliquey dolor do met
-                  sent. RELIT official consequent door ENIM RELIT Mollie.
-                  Excitation venial consequent sent nostrum met.
-                </h6>
-              </div>
-            </div>
-            <div className="flex gap-2 px-16 py-8">
+            <div className="flex gap-2 lg:px-16 py-8">
               <MdKeyboardArrowRight className="text-[#23A6F0] w-25 h-25" />
               <div className="flex flex-col gap-2">
                 <h5 className="font-bold leading-6 tracking-[0.1px] text-[#252B42]">
@@ -69,8 +41,8 @@ const PricingDescription = () => {
               </div>
             </div>
           </div>
-          <div className="flex items-center gap-8">
-            <div className="flex gap-2 px-16 py-8">
+          <div className="flex flex-col lg:flex lg:flex-row lg:items-center gap-8">
+            <div className="flex gap-2 lg:px-16 py-8">
               <MdKeyboardArrowRight className="text-[#23A6F0] w-25 h-25" />
               <div className="flex flex-col gap-2">
                 <h5 className="font-bold leading-6 tracking-[0.1px] text-[#252B42]">
@@ -83,7 +55,35 @@ const PricingDescription = () => {
                 </h6>
               </div>
             </div>
-            <div className="flex gap-2 px-16 py-8">
+            <div className="flex gap-2 lg:px-16 py-8">
+              <MdKeyboardArrowRight className="text-[#23A6F0] w-25 h-25" />
+              <div className="flex flex-col gap-2">
+                <h5 className="font-bold leading-6 tracking-[0.1px] text-[#252B42]">
+                  the quick fox jumps over the lazy dog
+                </h5>
+                <h6 className="text-[#737373] font-medium leading-5 tracking-[0.2px]">
+                  Met minim Mollie non desert Alamo est sit cliquey dolor do met
+                  sent. RELIT official consequent door ENIM RELIT Mollie.
+                  Excitation venial consequent sent nostrum met.
+                </h6>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col lg:flex lg:flex-row lg:items-center gap-8">
+            <div className="flex gap-2 lg:px-16 py-8">
+              <MdKeyboardArrowRight className="text-[#23A6F0] w-25 h-25" />
+              <div className="flex flex-col gap-2">
+                <h5 className="font-bold leading-6 tracking-[0.1px] text-[#252B42]">
+                  the quick fox jumps over the lazy dog
+                </h5>
+                <h6 className="text-[#737373] font-medium leading-5 tracking-[0.2px]">
+                  Met minim Mollie non desert Alamo est sit cliquey dolor do met
+                  sent. RELIT official consequent door ENIM RELIT Mollie.
+                  Excitation venial consequent sent nostrum met.
+                </h6>
+              </div>
+            </div>
+            <div className="flex gap-2 lg:px-16 py-8">
               <MdKeyboardArrowRight className="text-[#23A6F0] w-25 h-25" />
               <div className="flex flex-col gap-2">
                 <h5 className="font-bold leading-6 tracking-[0.1px] text-[#252B42]">
@@ -98,7 +98,7 @@ const PricingDescription = () => {
             </div>
           </div>
         </div>
-        <div className="flex items-center justify-center py-6">
+        <div className="flex items-center justify-center py-6 text-center lg:text-start">
           <p className="font-medium text-xl leading-6 tracking-[0.2px] text-[#737373]">
             Haven’t got your answer? Contact our support
           </p>
