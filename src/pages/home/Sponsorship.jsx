3b4d80a94b0c121f901f5fbe3606ef2a91f3532d
MuhammedZeki <muhammed.codes@gmail.com>
2025-12-03 15:22:41 +0300

    Sponsorshipt fetch data fn

--- src/pages/home/Sponsorship.jsx
+++ src/pages/home/Sponsorship.jsx
@@ -6,16 +6,39 @@ import {
   FaLyft,
 } from "react-icons/fa";
 import { DiEnvato } from "react-icons/di";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+
+const iconMap = {
+  FaAws,
+  FaRedditAlien,
+  FaStripe,
+  FaHooli,
+  FaLyft,
+  DiEnvato,
+};
+
 const Sponsorship = () => {
+  const getSponsorshipData = async () => {
+    const res = await axios.get("/db.json");
+    return res.data.Home.sponsorship;
+  };
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["sponsorship"],
+    queryFn: getSponsorshipData,
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Error...</p>;
+
   return (
     <div className=" bg-[#FAFAFA]">
       <div className="lg:flex lg:flex-row lg:gap-4 w-[75%] mx-auto lg:py-24 lg:px-4 flex flex-col py-10 items-center lg:items-start">
-        <FaHooli className="text-[#737373] h-30 w-30 grow" />
-        <FaLyft className="text-[#737373] h-30 w-30 grow" />
-        <DiEnvato className="text-[#737373] h-30 w-30 grow" />
-        <FaStripe className="text-[#737373] h-30 w-30 grow" />
-        <FaAws className="text-[#737373] h-30 w-30 grow" />
-        <FaRedditAlien className="text-[#737373] h-30 w-30 grow" />
+        {data.map((item, i) => {
+          const Icon = iconMap[item.icon];
+          return <Icon key={i} className="text-[#737373] h-30 w-30 grow" />;
+        })}
       </div>
     </div>
   );
