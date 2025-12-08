9c949386d19b7fb8381db3d1ca82c204f58405df
MuhammedZeki <muhammed.codes@gmail.com>
2025-12-08 20:01:33 +0300

    fixed team title-name is done

--- src/components/team/TeamItem.jsx
+++ src/components/team/TeamItem.jsx
@@ -5,10 +5,10 @@ const TeamItem = ({ img, name, job }) => {
     <div className="flex flex-col gap-3">
       <img src={img} className="w-full rounded-lg" />
       <div className="flex flex-col gap-2">
-        <h5 className="font-normal text-xl leading-8 tracking-[0.2px] text-[#252B42]">
+        <h5 className="font-normal text-xl leading-8 tracking-[0.2px] text-[#252B42] dark:text-[#fafafa]">
           {name}
         </h5>
-        <p className="font-normal text-sm leading-8 tracking-[0.2px] text-[#737373]">
+        <p className="font-normal text-sm leading-8 tracking-[0.2px] text-[#737373] dark:text-[#fafafa]">
           {job}
         </p>
       </div>
