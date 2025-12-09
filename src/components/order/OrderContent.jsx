50376d941bcff1c5019723f5a7c20c08b3a32b21
MuhammedZeki <muhammed.codes@gmail.com>
2025-12-09 17:53:08 +0300

    Order responsive is done

--- src/components/order/OrderContent.jsx
+++ src/components/order/OrderContent.jsx
@@ -6,13 +6,13 @@ import { IoIosArrowForward } from "react-icons/io";
 const OrderContent = () => {
   const { count, increment, descrement } = useContext(CounterContextt);
   return (
-    <div className=" w-full px-10 lg:w-[75%] mx-auto py-8 my-10  flex gap-4">
-      <div className=" w-3/4 flex flex-col gap-6 font-montserrat">
+    <div className=" w-full px-10 lg:w-[75%] mx-auto py-8 my-10  lg:flex lg:flex-row gap-4 flex flex-col ">
+      <div className="w-full lg:w-3/4 flex flex-col gap-6 font-montserrat">
         <div className=" flex flex-col gap-4">
           <h2 className="font-medium text-2xl leading-6 tracking-[0.1px] text-[#252B42] ">
             Sepetim (2 Ürün)
           </h2>
-          <div className="flex items-center bg-[#f5f8ff] py-4 px-2 rounded-sm gap-2">
+          <div className="sm:flex sm:flex-row items-center flex flex-col bg-[#f5f8ff] py-4 px-2 rounded-sm gap-2">
             <div>
               <FaCheckCircle className="text-[#0bbf5c] w-6 h-6" />
             </div>
@@ -23,15 +23,15 @@ const OrderContent = () => {
           </div>
         </div>
         <div className=" flex flex-col gap-10">
-          <div className="flex items-center gap-4 ">
-            <div className=" grow flex items-start gap-4">
-              <div className=" ">
+          <div className="md:flex md:flex-row md:items-center flex flex-col md:gap-4 gap-6 ">
+            <div className=" grow sm:flex sm:flex-row md:items-start flex flex-col gap-4 ">
+              <div className="flex  justify-center sm:justify-start">
                 <img
                   src="/shop/item1.jpg"
-                  className="object-cover w-[200px] rounded-md"
+                  className="object-cover w-[200px] sm:w-[200px] rounded-md"
                 />
               </div>
-              <div className=" grow flex flex-col gap-2">
+              <div className=" grow flex flex-col items-center sm:items-start gap-2">
                 <p className="font-normal text-[#737373] leading-6 tracking-[0.1px] max-w-[350px]">
                   <span className="font-bold text-[#252B42] text-lg leading-6 tracking-[0.1px] ">
                     Sütlü çikolatlı kek
@@ -81,15 +81,15 @@ const OrderContent = () => {
               </div>
             </div>
           </div>
-          <div className="flex items-center gap-4 ">
-            <div className=" grow flex items-start gap-4">
-              <div className=" ">
+          <div className="md:flex md:flex-row md:items-center flex flex-col md:gap-4 gap-6 ">
+            <div className=" grow sm:flex sm:flex-row md:items-start flex flex-col gap-4 ">
+              <div className="flex  justify-center sm:justify-start">
                 <img
                   src="/shop/item1.jpg"
-                  className="object-cover w-[200px] rounded-md"
+                  className="object-cover w-[200px] sm:w-[200px] rounded-md"
                 />
               </div>
-              <div className=" grow flex flex-col gap-2">
+              <div className=" grow flex flex-col items-center sm:items-start gap-2">
                 <p className="font-normal text-[#737373] leading-6 tracking-[0.1px] max-w-[350px]">
                   <span className="font-bold text-[#252B42] text-lg leading-6 tracking-[0.1px] ">
                     Sütlü çikolatlı kek
@@ -141,7 +141,7 @@ const OrderContent = () => {
           </div>
         </div>
       </div>
-      <div className=" w-1/4 flex flex-col font-montserrat gap-4">
+      <div className="w-full lg:w-1/4 flex flex-col font-montserrat gap-4">
         <div className="rounded-md border border-[#737373] flex flex-col gap-10 px-3 py-5">
           <div className="">
             <h2 className="font-medium text-2xl leading-6 tracking-[0.1px] text-[#252B42]">
